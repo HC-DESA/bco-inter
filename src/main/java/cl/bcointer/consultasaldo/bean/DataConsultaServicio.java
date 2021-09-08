9a136a4815df841fca584a4cc9f8456c3df0d7eb
Hernán Castro <hc.desa.git@gmail.com>
2021-09-08 14:17:13 -0300

    Se agrega bypass entre recepcion y llamda SOAP Se agrega log para identificar parametros de lectura desde  secret y cm

--- src/main/java/cl/bcointer/consultasaldo/bean/DataConsultaServicio.java
+++ src/main/java/cl/bcointer/consultasaldo/bean/DataConsultaServicio.java
@@ -1,11 +1,22 @@
 package cl.bcointer.consultasaldo.bean;
 
 import org.apache.camel.Exchange;
+import org.jboss.logging.Logger;
 
 public class DataConsultaServicio {
+    private static final Logger logger = Logger.getLogger("LoadData");
 
 	public void loadDataCM(Exchange exchange) {
-		exchange.setProperty("CODIGO-SERVICIO", System.getenv().getOrDefault("COD-SERVICIO", "No existe en property"));
-		exchange.setProperty("ID-EMISOR", System.getenv().getOrDefault("ID-EMISOR", "No existe en property"));
-		}
+		exchange.setProperty("CODIGO-SERVICIO", System.getenv().getOrDefault("COD-SERVICIO", "No existe en CONFIG-MAP"));
+		exchange.setProperty("ID-EMISOR", System.getenv().getOrDefault("ID-EMISOR", "No existe en CONFIG-MAP"));
+		exchange.setProperty("USER-CALL-SOAP", System.getenv().getOrDefault("USER-CALL-SOAP", "No existe en SECRET"));
+		
+		logger.info(">>> CODIGO-SERVICIO : " + exchange.getProperty("CODIGO-SERVICIO"));
+		logger.info(">>> ID-EMISOR : " + exchange.getProperty("CODIGO-SERVICIO"));
+		logger.info(">>> USER-CALL-SOAP : " + exchange.getProperty("CODIGO-SERVICIO"));
+		
+		
+		
+
+	}
 }
